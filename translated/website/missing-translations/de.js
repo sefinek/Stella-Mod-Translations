85c937360fc4e661ee91ac2acbdb3a4f7ef5d9e4
Sefinek <sefinek5916@gmail.com>
2025-03-01 20:49:52 +0100

    New translations en.js (German)

---
+++ translated/website/missing-translations/de.js
@@ -0,0 +1,13 @@
+exports.data = {
+ Version: '2.0.2',
+ Contributors: ['Sefinek <contact@sefinek.net> (https://sefinek.net)'],
+};
+module.exports = {
+ OhNoSomethingWentWrong: 'Oh nein! Etwas ist schief gelaufen...',
+ WereSorryButWeCouldFindTheCorrectTranslation:
+  'Entschuldigung, wir konnten leider keine Übersetzung für deine Sprache {0} finden. Für mehr Details prüfe bitte die Konsole. (≧◡≦) ♡',
+ IfYouWouldLikeToContributeYourOwnTranslationOrHelpUsFixThisIssue:
+  'Wenn du uns helfen möchtest und eine eigene Übersetzung bereitstellen willst, erstelle bitte einen {0}Pull Request{/0} auf unserem {1}GitHub Repository{/1}. Vielen Dank für deine Hilfe! o(>ω<)o',
+ SwitchLanguageToEnglish: 'Sprache zu Englisch wechseln',
+ YesClickMe: 'Ja, klick mich!',
+};
