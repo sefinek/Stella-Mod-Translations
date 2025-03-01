6ca2a34141244a0e843819df60c184162c501306
Sefinek <sefinek5916@gmail.com>
2025-03-01 21:13:28 +0100

    New translations en.js (Polish)

---
+++ translated/website/missing-translations/en.pl.js
@@ -0,0 +1,13 @@
+exports.data = {
+ Version: '2.0.2',
+ Contributors: ['Sefinek <contact@sefinek.net> (https://sefinek.net)'],
+};
+module.exports = {
+ OhNoSomethingWentWrong: 'O nie! Coś poszło nie tak...',
+ WereSorryButWeCouldFindTheCorrectTranslation:
+  'Przepraszamy, ale nie mogliśmy znaleźć prawidłowego tłumaczenia dla Twojego języka - {0}. Aby uzyskać więcej informacji, sprawdź szczegóły konsoli. (≧◡≦) ♡',
+ IfYouWouldLikeToContributeYourOwnTranslationOrHelpUsFixThisIssue:
+  'Jeśli chcesz przesłać własne tłumaczenie lub pomóc nam rozwiązać ten problem, utwórz nowy {0}Pull Request{/0} na naszym {1}repozytorium GitHub{/1}. Dzięki za pomoc! o(>ω<)o',
+ SwitchLanguageToEnglish: 'Zmień język na angielski',
+ YesClickMe: 'Takk, kliknij mnie!',
+};
