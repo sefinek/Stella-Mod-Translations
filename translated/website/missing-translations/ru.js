c0038fe44cd0cf5598227c9aa671342abd04348a
Sefinek <contact@sefinek.net>
2025-03-03 11:56:23 +0100

    Revert "Cleanup"
    This reverts commit e130c1bd62814fa46869be9ee33b98cf60c8f4ff.

---
+++ translated/website/missing-translations/ru.js
@@ -0,0 +1,13 @@
+exports.data = {
+ Version: '2.0.2',
+ Contributors: ['Sefinek <contact@sefinek.net> (https://sefinek.net)'],
+};
+module.exports = {
+ OhNoSomethingWentWrong: 'О нет! Что-то пошло не так...',
+ WereSorryButWeCouldFindTheCorrectTranslation:
+  'Извините, мы не смогли найти правильный перевод для вашего языка - {0}. Для получения дополнительной информации, пожалуйста, проверьте детали консоли. (≧◡≦) ♡',
+ IfYouWouldLikeToContributeYourOwnTranslationOrHelpUsFixThisIssue:
+  'Если вы хотите предоставить свой перевод или помочь нам исправить эту проблему, пожалуйста, создайте новый {0}Pull Request{/0} на нашем {1}GitHub репозитории{/1}. Спасибо за помощь! o(>ω<)o',
+ SwitchLanguageToEnglish: 'Переключите язык на английский',
+ YesClickMe: 'Да, нажмите на меня!',
+};
