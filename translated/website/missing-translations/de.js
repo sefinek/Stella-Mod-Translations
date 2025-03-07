235f8f228b01c861dfda60ddc148bceb775e7b74
Sefinek <sefinek5916@gmail.com>
2025-03-07 20:26:34 +0100

    New translations en.js (German)

--- translated/website/missing-translations/de.js
+++ translated/website/missing-translations/de.js
@@ -1,9 +1,8 @@
 module.exports = {
  OhNoSomethingWentWrong: 'Oh nein! Etwas ist schief gelaufen...',
  WereSorryButWeCouldFindTheCorrectTranslation:
-  'Entschuldigung, wir konnten leider keine Übersetzung für deine Sprache {0} finden. Für mehr Details prüfe bitte die Konsole. (≧◡≦) ♡',
- IfYouWouldLikeToContributeYourOwnTranslationOrHelpUsFixThisIssue:
-  'Wenn du uns helfen möchtest und eine eigene Übersetzung bereitstellen willst, erstelle bitte einen {0}Pull Request{/0} auf unserem {1}GitHub Repository{/1}. Vielen Dank für deine Hilfe! o(>ω<)o',
+  "Sorry, but we couldn't find translations for the {0} language. For more information, check the details in the console.",
+ IfYouWoldLikeToAddYourOwnTranslations:
+  'If you would like to add your own translation or help us fix this issue, check out our {0}Crowdin{/0} page. Thank you for your help! (≧◡≦) ♡',
  SwitchLanguageToEnglish: 'Sprache zu Englisch wechseln',
- YesClickMe: 'Ja, klick mich!',
 };
