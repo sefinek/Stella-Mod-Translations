87dd95f8567a09e7f890fccaddbbebf89464e099
Sefinek <sefinek5916@gmail.com>
2025-03-07 20:32:01 +0100

    New translations en.js (Turkish)

--- translated/website/missing-translations/tr.js
+++ translated/website/missing-translations/tr.js
@@ -1,8 +1,8 @@
 module.exports = {
  OhNoSomethingWentWrong: 'Olamaz! Bir şeyler ters gitti...',
  WereSorryButWeCouldFindTheCorrectTranslation:
-  "Sorry, but we couldn't find translations for the {0} language. For more information, check the details in the console.",
+  'Üzgünüz, {0} dili için çeviri bulamadık. Daha fazla bilgi için konsoldaki ayrıntıları kontrol edin.',
  IfYouWoldLikeToAddYourOwnTranslations:
-  'If you would like to add your own translation or help us fix this issue, check out our {0}Crowdin{/0} page. Thank you for your help! (≧◡≦) ♡',
+  'Kendi çevirinizi eklemek veya bu sorunu çözmemize yardımcı olmak isterseniz {0}Crowdin{/0} sayfamıza göz atın. Yardımlarınız için teşekkür ederiz! (≧◡≦) ♡',
  SwitchLanguageToEnglish: 'Dili İngilizce olarak değiştir',
 };
