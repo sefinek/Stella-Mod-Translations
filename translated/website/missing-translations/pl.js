6477b889faec63e47e7dd5ab5079f2f7e0ec45d4
Sefinek <sefinek5916@gmail.com>
2025-03-11 18:14:58 +0100

    New translations en.js (Polish)

---
+++ translated/website/missing-translations/pl.js
@@ -0,0 +1,8 @@
+module.exports = {
+ OhNoSomethingWentWrong: 'O nie! Coś poszło nie tak...',
+ WereSorryButWeCouldFindTheCorrectTranslation:
+  "Sorry, but we couldn't find translations for the {0} language. For more information, check the details in the console.",
+ IfYouWoldLikeToAddYourOwnTranslations:
+  'If you would like to add your own translation or help us fix this issue, check out our {0}Crowdin{/0} page. Thank you for your help! (≧◡≦) ♡',
+ SwitchLanguageToEnglish: 'Zmień język na angielski',
+};
