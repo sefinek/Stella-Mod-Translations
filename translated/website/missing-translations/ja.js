a41e6ce8f8a8c95a34404582ec693ccde1933cdd
Sefinek <contact@sefinek.net>
2025-03-11 18:54:41 +0100

    Update

--- translated/website/missing-translations/ja.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
- OhNoSomethingWentWrong: '「あらら、何かがうまくいかなかったようですね...',
- WereSorryButWeCouldFindTheCorrectTranslation:
-  "Sorry, but we couldn't find translations for the {0} language. For more information, check the details in the console.",
- IfYouWoldLikeToAddYourOwnTranslations:
-  'If you would like to add your own translation or help us fix this issue, check out our {0}Crowdin{/0} page. Thank you for your help! (≧◡≦) ♡',
- SwitchLanguageToEnglish: '言語を英語に切り替える',
-};
