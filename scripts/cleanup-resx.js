28fe798666acf9cce89a608ed1e89b79db5c24ec
Sefinek <contact@sefinek.net>
2025-04-03 17:15:51 +0200

    Update

---
+++ scripts/cleanup-resx.js
@@ -0,0 +1,9 @@
+const resxCleanup = require('resx-cleanup');
+
+resxCleanup([
+	'D:\\Projects\\stella\\Genshin-Impact-ReShade\\Stella.Launcher',
+	'D:\\Projects\\stella\\Genshin-Impact-ReShade\\Stella.Configuration',
+	'D:\\Projects\\stella\\Genshin-Impact-ReShade\\Stella.InfoBeforeStart',
+	'D:\\Projects\\stella\\Genshin-Impact-ReShade\\Stella.Worker',
+	'D:\\Projects\\stella\\Genshin-FPS-Unlocker',
+]);
